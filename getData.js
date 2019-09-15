e939c1df199af120db981a4ddecfbdcba2250e8d
walek-itfox <55052157+walek-itfox@users.noreply.github.com>
2019-09-15 07:41:25 +0300

    Add files via upload

--- getData.js
+++ getData.js
@@ -2,8 +2,9 @@ const request = require("request");
 
 const path = "https://iss.moex.com/iss/engines/stock/markets/shares/securities/AFLT.json";
 
-module.exports.data = function() {
-  request(path, (err, res, body) => {
+module.exports.data = async function() {
+  await request(path, (err, res, body) => {
+    console.log(JSON.parse(body).marketdata.data);
     return JSON.parse(body).marketdata.data;
   });
 }
